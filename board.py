4d6e986bd85502e2bfa2c9e8d4bd4cd0a46248ef
Ayedux <aidanjw05@gmail.com>
2020-11-22 09:53:18 +1300

    Added documentation

--- board.py
+++ board.py
@@ -3,7 +3,11 @@ board = []
 
 def create_board():
     """
+    A function that creates the board object.\n
+
+    Creates a 8x8 grid of list which contain the x and y coordinates as well as the piece occupying the square.
     """
+
     for x in range(1, BOARDSIZE+1):
         row = []
         for y in range(1, BOARDSIZE+1):
@@ -11,6 +15,14 @@ def create_board():
         board.append(row)
 
 def printannotatedboard():
+    """
+    A function that prints the board showing the piece occupying the square with notation printed along the side of the board.\n
+
+     - At the start of every row it prints the row number.
+     - At the bottom of every column it prints the column letter.
+     - Prints the 3rd item (piece) in the square list.
+    """
+
     letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
     rownum = 1
     for row in board:
@@ -19,6 +31,7 @@ def printannotatedboard():
             print(square[2], end =" ")
         rownum += 1
         print("")
+        
     #Prints the file letter
     print(" ", end =" ")
     for i in range(BOARDSIZE):            

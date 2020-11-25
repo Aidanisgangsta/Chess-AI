c788c5d352be12f78c98e5f652daf83d4c7ed888
Ayedux <aidanjw05@gmail.com>
2020-11-25 15:57:48 +1300

    Added documentation to make_move function

--- chess-ai.py
+++ chess-ai.py
@@ -5,6 +5,12 @@ import re
 
 def make_move():
     """
+    Function which a user enters move and it is checked whether it meets the correct format.\n
+
+     - Checks whether the move entered is the correct length (4 characters).
+     - Then checks if the move entered fits the correct format.
+      - Checks whether the first and third characters of the move entered are between a and h.
+      - Check whether the second and fourth characters of the move entered are between 1 and 8.
     """
     while True:
         move = input("\nPlease enter a move: ")
@@ -13,7 +19,7 @@ def make_move():
             #Checks if move is on the board
             valid_move = bool(re.match(r"[a-h][1-8][a-h][1-8]", move))
             if valid_move == True:
-                break
+                pass
             else:
                 print("\nPlease enter a move in the correct format (e.g. a1c3)")
         else:

07e889ed60d64fc128e473f98f20dc03a7764162
Ayedux <aidanjw05@gmail.com>
2020-12-02 18:13:28 +1300

    Added 50 move rule

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import board
 import moves
 import chess_ai
 
+halfmove_checker = 0
+
 def threefold_check() -> bool:
     """
     A function that checks if a board state has occured three times.\n
@@ -21,9 +23,16 @@ def main():
     board.printboard()
     while True:
         moves.make_move()
+        halfmove_checker += 1
+
         #Checks for threefold repition
         if threefold_check():
             print("\nDraw by threefold repition")
+            break
+        
+        #Checks for 50 move rule
+        if halfmove_checker == 100:
+            print("\nDraw by 50 move rule")
             break
 
 main()

679f132949bdb81020bfd5b3446c70bee5cff8e7
Ayedux <aidanjw05@gmail.com>
2020-12-28 01:03:52 +1300

    Created random move maker

--- chess_ai.py
+++ chess_ai.py
@@ -1,2 +1,24 @@
+from board import chessboard
+from moves import whos_move, piece_moves, move_checker
+from random import choice
+
 #Piece-value tables and piece values
-piece_values = {"p": 10, "n": 30, "b": 32, "r": 51, "q": 90, "k": 999999}
+piece_values = {"p": 100, "n": 300, "b": 320, "r": 510, "q": 900, "k": 999999}
+
+def random_move_gen():
+    moves = []
+    #Finds all the possible moves for the player
+    for i in range(len(chessboard)):
+        if whos_move == 1:
+            if chessboard[i].isupper():
+                for move in piece_moves[chessboard[i].casefold()]:
+                    moves.append((i, i + (move * whos_move), chessboard[i]))
+        elif whos_move == -1:
+            if chessboard[i].islower():
+                for move in piece_moves[chessboard[i].casefold()]:
+                    moves.append((i, i + (move * whos_move), chessboard[i]))
+    while True:
+        rnd_move = choice(moves)
+        move_checker(rnd_move[0], rnd_move[1], rnd_move[2])
+
+random_move_gen()

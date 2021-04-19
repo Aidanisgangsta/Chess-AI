d2e018c26bdc7f04bf09a056d2b242f900d329d1
Ayedux <aidanjw05@gmail.com>
2021-04-20 02:23:07 +1200

    Add move_validator() function
    Checks to see if the user entered move meets the correct format

--- src/move_checker.py
+++ src/move_checker.py
@@ -4,3 +4,23 @@ import piece_check as pc
 
 import re
 
+def move_vaildator(move: str) -> bool:
+    """
+    A function that checks if the users move meets the correct format.\n
+
+     - Checks to if the move is 4 characters long.
+     - Then checks if the move meets the correct format.
+         - Checks if the move contains 2 squares on the board.
+    """
+
+    if len(move) == 4:
+        # Checks if the move entered is vaild
+        valid_move = bool(re.match(r"[a-h][1-8][a-h][1-8]", move))
+        if valid_move:
+            return True
+        else:
+            print("Please enter a move in the correct format (e.g. b1c3)")
+            return False
+    else:
+        print("Please enter a valid move")
+        return False
